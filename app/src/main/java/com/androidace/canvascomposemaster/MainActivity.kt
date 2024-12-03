b69cc72a6a5e5e3dbb5d7f8aa3561143a296c097
devAseemSharma <er.sharmaaseem@gmail.com>
2024-12-03 14:35:37 +0530

    Implemented ball clicker game

--- app/src/main/java/com/androidace/canvascomposemaster/MainActivity.kt
+++ app/src/main/java/com/androidace/canvascomposemaster/MainActivity.kt
@@ -4,26 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.androidace.canvascomposemaster.ui.theme.CanvasComposeMasterTheme
+import kotlinx.coroutines.delay
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CanvasComposeMasterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen()
                 }
             }
         }
@@ -31,17 +52,126 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier) {
+    var points by remember {
+        mutableStateOf(0)
+    }
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+
+        ) {
+            Text(
+                text = "Points: $points", fontSize = 20.sp, fontWeight = FontWeight.Bold
+            )
+            Button(onClick = {
+                isTimerRunning = !isTimerRunning
+                points = 0
+            }) {
+                Text(text = if (isTimerRunning) "Reset" else "Start")
+            }
+            CountDownTimer(isTimerRunning = isTimerRunning) {
+                isTimerRunning = false
+            }
+        }
+        BallClicker(
+            enabled = isTimerRunning
+        ) {
+            points++
+        }
+    }
+}
+
+@Composable
+fun CountDownTimer(
+    time: Int = 30000, isTimerRunning: Boolean = false, onTimerEnd: () -> Unit
+) {
+    var curTime by remember {
+        mutableStateOf(time)
+    }
+    LaunchedEffect(key1 = curTime, key2 = isTimerRunning) {
+
+        if (!isTimerRunning) {
+            curTime = time
+            return@LaunchedEffect
+        }
+        if (curTime > 0) {
+            delay(1000L)
+            curTime -= 1000
+        } else {
+            onTimerEnd()
+        }
+    }
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = (curTime / 1000).toString(),
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
     )
 }
+
+@Composable
+fun BallClicker(
+    radius: Float = 100f,
+    enabled: Boolean = false,
+    ballColor: Color = Color.Red,
+    onBallClick: () -> Unit
+) {
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        var ballPosition by remember {
+            mutableStateOf(
+                randomOffset(
+                    radius = radius,
+                    width = constraints.maxWidth,
+                    height = constraints.maxHeight,
+                )
+            )
+        }
+        Canvas(modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(enabled) {
+                if (!enabled) {
+                    return@pointerInput
+                }
+                detectTapGestures {
+                    val distance =
+                        sqrt((it.x - ballPosition.x).pow(2) + (it.y - ballPosition.y).pow(2))
+                    if (distance <= radius) {
+                        ballPosition = randomOffset(
+                            radius = radius,
+                            width = constraints.maxWidth,
+                            height = constraints.maxHeight
+                        )
+                        onBallClick()
+                    }
+                }
+            }
+        ) {
+
+            drawCircle(color = ballColor, radius = radius, center = ballPosition)
+        }
+    }
+
+}
+
+private fun randomOffset(radius: Float, width: Int, height: Int): Offset {
+    return Offset(
+        x = Random.nextInt(radius.roundToInt(), width - radius.roundToInt()).toFloat(),
+        y = Random.nextInt(radius.roundToInt(), height - radius.roundToInt()).toFloat()
+    )
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CanvasComposeMasterTheme {
-        Greeting("Android")
+
     }
 }
