d08843c623e7fc8f423d7f4721c15e49c5b9c23f
devAseemSharma <er.sharmaaseem@gmail.com>
2025-11-30 15:57:46 +0530

    Path operations and animations

--- app/src/main/java/com/androidace/canvascomposemaster/MainActivity.kt
+++ app/src/main/java/com/androidace/canvascomposemaster/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.androidace.canvascomposemaster
 
+import android.R
 import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,11 +29,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathMeasure
+import androidx.compose.ui.graphics.PathOperation
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.draw
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_7_PRO
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +59,8 @@ import com.androidace.canvascomposemaster.ui.theme.DarkGreen
 import com.androidace.canvascomposemaster.weightpicker.Scale
 import com.androidace.canvascomposemaster.weightpicker.ScaleStyle
 import kotlinx.coroutines.delay
+import kotlin.math.PI
+import kotlin.math.atan2
 import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
@@ -48,10 +69,131 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CanvasComposeMasterTheme {
-                ComposeClockScreen()
+                CanvasPathBasic()
             }
         }
     }
+}
+
+@Composable
+private fun CanvasPathBasic() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val path = Path().apply {
+            moveTo(1000f, 100f)
+            lineTo(100f, 500f)
+            lineTo(500f, 500f)
+            //quadraticBezierTo(x1 = 800f, y1 = 300f, x2 = 500f, y2 = 100f)
+            cubicTo(800f, 500f, 800f, 100f, 500f, 100f)
+            //close()
+        }
+        drawPath(
+            path = path,
+            color = Color.Red,
+            style = Stroke(
+                width = 10.dp.toPx(),
+                cap = StrokeCap.Round, // Makes the end of the line as rounded
+                join = StrokeJoin.Round, // Makes the joint of the stroke as rounded or whatever value added
+                miter = 5f // Control how much we want to cutoff when angle is sharper
+            )
+        )
+    }
+}
+
+@Composable
+private fun PathOperations() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val squaredWithoutOp = Path().apply {
+            addRect(Rect(Offset(200f, 200f), Size(200f, 200f)))
+        }
+
+        val circle = Path().apply {
+            addOval(Rect(Offset(200f, 200f), 100f))
+        }
+
+        val pathWithOp = Path().apply {
+            op(squaredWithoutOp, circle, PathOperation.Xor)
+        }
+
+        drawPath(
+            path = squaredWithoutOp,
+            color = Color.Red,
+            style = Stroke(width = 2.dp.toPx())
+        )
+
+        drawPath(
+            path = circle,
+            color = Color.Blue,
+            style = Stroke(width = 2.dp.toPx())
+        )
+
+        drawPath(
+            path = pathWithOp,
+            color = Color.Green,
+            style = Fill
+        )
+    }
+}
+
+@Composable
+fun PathAnimation(modifier: Modifier = Modifier) {
+    val pathPortion = remember {
+        Animatable(initialValue = 0f)
+    }
+    LaunchedEffect(true) {
+        pathPortion.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 5000
+            )
+        )
+    }
+    val path = Path().apply {
+        moveTo(100f, 100f)
+        quadraticBezierTo(400f, 400f, 100f, 400f)
+    }
+    /*val outPath = Path()
+    PathMeasure().apply {
+        setPath(path, false)
+        getSegment(0f, pathPortion.value * length, outPath)
+    }*/
+    val outPath = android.graphics.Path()
+    val pos = FloatArray(2)
+    val tan = FloatArray(2)
+    android.graphics.PathMeasure().apply {
+        setPath(path.asAndroidPath(), false)
+        getSegment(0f, pathPortion.value * length, outPath, true)
+        getPosTan(pathPortion.value * length, pos, tan)
+    }
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        drawPath(
+            path = outPath.asComposePath(),
+            color = Color.Red,
+            style = Stroke(width = 5.dp.toPx())
+        )
+        val x = pos[0]
+        val y = pos[1]
+        val degrees = -atan2(tan[0], tan[1]) * (180f / PI.toFloat()) - 180f
+        rotate(degrees, pivot = Offset(x, y)) {
+            drawPath(
+                path = Path().apply {
+                    moveTo(x, y - 30f)
+                    lineTo(x - 30f, y + 60f)
+                    lineTo(x + 30f, y + 60f)
+                    close()
+                },
+                color = Color.Red
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, device = PIXEL_7_PRO, showSystemUi = true)
+@Composable
+private fun CanvasPathBasicPreview() {
+    //CanvasPathBasic()
+    //PathOperations()
+    PathAnimation()
 }
 
 @Composable
--- app/src/main/java/com/androidace/canvascomposemaster/clock/ComposeClock.kt
+++ app/src/main/java/com/androidace/canvascomposemaster/clock/ComposeClock.kt
@@ -9,17 +9,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.withRotation
-import com.androidace.canvascomposemaster.weightpicker.LineType
 import kotlin.math.PI
-import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sin
 
