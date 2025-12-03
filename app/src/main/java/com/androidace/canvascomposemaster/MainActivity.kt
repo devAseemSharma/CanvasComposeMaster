180c3aee6c3e483805fd03493f271e11a611572b
devAseemSharma <er.sharmaaseem@gmail.com>
2025-12-03 19:03:27 +0530

    Path Effect sample

--- app/src/main/java/com/androidace/canvascomposemaster/MainActivity.kt
+++ app/src/main/java/com/androidace/canvascomposemaster/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
@@ -32,8 +36,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.PathOperation
 import androidx.compose.ui.graphics.StrokeCap
@@ -219,12 +225,106 @@ fun ClipPathSample(modifier: Modifier = Modifier) {
     }
 }
 
+/**
+ *    val phase by infiniteTransition.animateFloat(
+ *         initialValue = 0f,
+ *         targetValue = 10000f,
+ *         animationSpec = InfiniteRepeatableSpec(
+ *             animation = tween(60000, easing = LinearEasing)
+ *         )
+ *     )
+ *           drawPath(
+ *               path = path,
+ *               color = Color.Red,
+ *               style = Stroke(
+ *                   width = 5.dp.toPx(),
+ *                   pathEffect = PathEffect.dashPathEffect(
+ *                       intervals = floatArrayOf(50f, 30f),
+ *                       phase = phase
+ *                   )
+ *               )
+ *           )
+
+ *    To make corners rounded without complicated calculations
+ *
+ *    @sample
+ *    drawPath(
+ *          path = path,
+ *          color = Color.Red,
+ *          style = Stroke(
+ *              width = 5.dp.toPx(),
+ *              pathEffect = PathEffect.cornerPathEffect(
+ *                  radius = 1000f
+ *              )
+ *          )
+ *      )
+ *    @sample
+ *
+ *    Stamped Path Effect
+ *
+ *    val oval = Path().apply {
+ *         addOval(Rect(topLeft = Offset.Zero, bottomRight = Offset(40f, 10f)))
+ *    }
+ *
+ *    drawPath(
+ *             path = path,
+ *             color = Color.Red,
+ *             style = Stroke(
+ *                 width = 5.dp.toPx(),
+ *                 pathEffect = PathEffect.stampedPathEffect(
+ *                     shape = oval,
+ *                     advance = 100f,
+ *                     phase = phase,
+ *                     style = StampedPathEffectStyle.Rotate // .Morph.translate
+ *                 )
+ *             )
+ *         )
+ *
+ *
+ *    Chained path effect
+ *
+ *    PathEffect.chainPathEffect() // to combine paths
+ *
+ * **/
+
+@Composable
+fun PathEffectsSample(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val phase by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 10000f,
+        animationSpec = InfiniteRepeatableSpec(
+            animation = tween(60000, easing = LinearEasing)
+        )
+    )
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val path = Path().apply {
+            moveTo(100f, 100f)
+            cubicTo(100f, 300f, 600f, 700f, 600f, 1100f)
+            lineTo(800f, 800f)
+            lineTo(1000f, 1100f)
+        }
+
+        drawPath(
+            path = path,
+            color = Color.Red,
+            style = Stroke(
+                width = 5.dp.toPx(),
+                pathEffect = PathEffect.cornerPathEffect(
+                    radius = 1000f
+                )
+            )
+        )
+    }
+}
+
 @Preview(showBackground = true, device = PIXEL_7_PRO, showSystemUi = true)
 @Composable
 private fun CanvasPathBasicPreview() {
     //CanvasPathBasic()
     //PathOperations()
-    PathAnimation()
+    //PathAnimation()
+    PathEffectsSample()
 }
 
 @Composable
